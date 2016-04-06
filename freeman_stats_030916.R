14ef7c693ecd7e357695b7cd24c285b2943d66fb
Ethan Linck <elinck@uw.edu>
2016-04-06 08:02:18 -0700

    exploratory models

--- freeman_stats_030916.R
+++ freeman_stats_030916.R
@@ -7,9 +7,11 @@ library("reshape"); library("plyr"); library("dplyr"); library("car"); library("
 
 setwd("~/Desktop/Freeman_stats/")
 
+
+
 #read in data
 a <- read.csv("data_final.csv")
-attach(a)
+#attach(a)
 head(a)
 
 #reverse code where necessary
@@ -21,36 +23,43 @@ a[,47] <- ifelse(a[,47] == 5, 1,
                                       ifelse(a[,47] == 1, 5, "NA"))))) 
 which(colnames(a)=="PostQ12a")
 a[,105] <- ifelse(a[,105] == 5, 1,
-                 ifelse(a[,105] == 4, 2,
-                        ifelse(a[,105] == 3, 3, 
-                               ifelse(a[,105] == 2, 4, 
-                                      ifelse(a[,105] == 1, 5, "NA"))))) 
+                  ifelse(a[,105] == 4, 2,
+                         ifelse(a[,105] == 3, 3, 
+                                ifelse(a[,105] == 2, 4, 
+                                       ifelse(a[,105] == 1, 5, "NA"))))) 
 which(colnames(a)=="PreQ12g")
 a[,53] <- ifelse(a[,53] == 5, 1,
-                  ifelse(a[,53] == 4, 2,
-                         ifelse(a[,53] == 3, 3, 
-                                ifelse(a[,53] == 2, 4, 
-                                       ifelse(a[,53] == 1, 5, "NA"))))) 
+                 ifelse(a[,53] == 4, 2,
+                        ifelse(a[,53] == 3, 3, 
+                               ifelse(a[,53] == 2, 4, 
+                                      ifelse(a[,53] == 1, 5, "NA"))))) 
 which(colnames(a)=="PostQ12g")
 a[,111] <- ifelse(a[,111] == 5, 1,
-                 ifelse(a[,111] == 4, 2,
-                        ifelse(a[,111] == 3, 3, 
-                               ifelse(a[,111] == 2, 4, 
-                                      ifelse(a[,111] == 1, 5, "NA"))))) 
+                  ifelse(a[,111] == 4, 2,
+                         ifelse(a[,111] == 3, 3, 
+                                ifelse(a[,111] == 2, 4, 
+                                       ifelse(a[,111] == 1, 5, "NA"))))) 
+a[,47] <- as.numeric(a[,47])
+as.numeric(a[,105])
+as.numeric(a[,53])
+as.numeric(a[,111])
 
 #melt and reshape
 b <- melt(a, id=c("ID", "section","Gender","URM","FirstGen","EOP","UW_GPA","Transfer","SATM","SATV", "Ex3tot"))
 head(b)
-attach(b)
-c <- b[order(ID),]
-head(c)
+#attach(b)
+#c <- b[order("ID"),]
 
+
+c <- b
+dim(c)
 
 # create "student of interest" ID column
 c$SOI <- ifelse(c$URM == 1, 1, 
                 ifelse(c$EOP == 1, 1,
                        ifelse(c$FirstGen == 1, 1, 0)))
 head(c)
+dim(c)
 
 # designate affect questions by cluster (1 = Effort Reg; 2 = Intrinsic Goal; 3 = Task Value)
 
@@ -87,70 +96,72 @@ n <- length(c$ID)
 InApp <- rep(0,n)
 for(i in 1:n)
 {
-if (c$variable[i] == "PreQ1a") InApp[i] <- 1
-if (c$variable[i] == "PreQ1b") InApp[i] <- 1 
-if (c$variable[i] == "PreQ2a") InApp[i] <- 1 
-if (c$variable[i] == "PreQ2b") InApp[i] <- 1 
-if (c$variable[i] == "PreQ2c") InApp[i] <- 1 
-if (c$variable[i] == "PreQ2d") InApp[i] <- 1  
-if (c$variable[i] == "PreQ4a") InApp[i] <- 1  
-if (c$variable[i] == "PreQ4b") InApp[i] <- 1   
-if (c$variable[i] == "PreQ4c") InApp[i] <- 1
-if (c$variable[i] == "PreQ4d") InApp[i] <- 1
-if (c$variable[i] == "PreQ5a") InApp[i] <- 1
-if (c$variable[i] == "PreQ6a") InApp[i] <- 1  
-if (c$variable[i] == "PreQ6b") InApp[i] <- 1 
-if (c$variable[i] == "PreQ6d") InApp[i] <- 1 
-if (c$variable[i] == "PreQ7a") InApp[i] <- 1
-if (c$variable[i] == "PreQ7b") InApp[i] <- 1
-if (c$variable[i] == "PreQ7c") InApp[i] <- 1
-if (c$variable[i] == "PreQ7d") InApp[i] <- 1
-if (c$variable[i] == "PreQ8a") InApp[i] <- 1
-if (c$variable[i] == "PreQ8c") InApp[i] <- 1
-if (c$variable[i] == "PreQ9a") InApp[i] <- 1
-if (c$variable[i] == "PreQ9b") InApp[i] <- 1
-if (c$variable[i] == "PreQ9c") InApp[i] <- 1
-if (c$variable[i] == "PreQ9d") InApp[i] <- 1
-if (c$variable[i] == "PreQ9e") InApp[i] <- 1
-if (c$variable[i] == "PreQ10a") InApp[i] <- 1
-if (c$variable[i] == "PreQ10b") InApp[i] <- 1
-if (c$variable[i] == "PreQ10c") InApp[i] <- 1
-if (c$variable[i] == "PreQ10d") InApp[i] <- 1
-if (c$variable[i] == "PostQ1a") InApp[i] <- 1
-if (c$variable[i] == "PostQ1b") InApp[i] <- 1 
-if (c$variable[i] == "PostQ2a") InApp[i] <- 1 
-if (c$variable[i] == "PostQ2b") InApp[i] <- 1 
-if (c$variable[i] == "PostQ2c") InApp[i] <- 1 
-if (c$variable[i] == "PostQ2d") InApp[i] <- 1  
-if (c$variable[i] == "PostQ4a") InApp[i] <- 1  
-if (c$variable[i] == "PostQ4b") InApp[i] <- 1   
-if (c$variable[i] == "PostQ4c") InApp[i] <- 1
-if (c$variable[i] == "PostQ4d") InApp[i] <- 1
-if (c$variable[i] == "PostQ5a") InApp[i] <- 1
-if (c$variable[i] == "PostQ6a") InApp[i] <- 1  
-if (c$variable[i] == "PostQ6b") InApp[i] <- 1 
-if (c$variable[i] == "PostQ6d") InApp[i] <- 1 
-if (c$variable[i] == "PostQ7a") InApp[i] <- 1
-if (c$variable[i] == "PostQ7b") InApp[i] <- 1
-if (c$variable[i] == "PostQ7c") InApp[i] <- 1
-if (c$variable[i] == "PostQ7d") InApp[i] <- 1
-if (c$variable[i] == "PostQ8a") InApp[i] <- 1
-if (c$variable[i] == "PostQ8c") InApp[i] <- 1
-if (c$variable[i] == "PostQ9a") InApp[i] <- 1
-if (c$variable[i] == "PostQ9b") InApp[i] <- 1
-if (c$variable[i] == "PostQ9c") InApp[i] <- 1
-if (c$variable[i] == "PostQ9d") InApp[i] <- 1
-if (c$variable[i] == "PostQ9e") InApp[i] <- 1
-if (c$variable[i] == "PostQ10a") InApp[i] <- 1
-if (c$variable[i] == "PostQ10b") InApp[i] <- 1
-if (c$variable[i] == "PostQ10c") InApp[i] <- 1
-if (c$variable[i] == "PostQ10d") InApp[i] <- 1
+  if (c$variable[i] == "PreQ1a") InApp[i] <- 1
+  if (c$variable[i] == "PreQ1b") InApp[i] <- 1 
+  if (c$variable[i] == "PreQ2a") InApp[i] <- 1 
+  if (c$variable[i] == "PreQ2b") InApp[i] <- 1 
+  if (c$variable[i] == "PreQ2c") InApp[i] <- 1 
+  if (c$variable[i] == "PreQ2d") InApp[i] <- 1  
+  if (c$variable[i] == "PreQ4a") InApp[i] <- 1  
+  if (c$variable[i] == "PreQ4b") InApp[i] <- 1   
+  if (c$variable[i] == "PreQ4c") InApp[i] <- 1
+  if (c$variable[i] == "PreQ4d") InApp[i] <- 1
+  if (c$variable[i] == "PreQ5a") InApp[i] <- 1
+  if (c$variable[i] == "PreQ6a") InApp[i] <- 1  
+  if (c$variable[i] == "PreQ6b") InApp[i] <- 1 
+  if (c$variable[i] == "PreQ6d") InApp[i] <- 1 
+  if (c$variable[i] == "PreQ7a") InApp[i] <- 1
+  if (c$variable[i] == "PreQ7b") InApp[i] <- 1
+  if (c$variable[i] == "PreQ7c") InApp[i] <- 1
+  if (c$variable[i] == "PreQ7d") InApp[i] <- 1
+  if (c$variable[i] == "PreQ8a") InApp[i] <- 1
+  if (c$variable[i] == "PreQ8c") InApp[i] <- 1
+  if (c$variable[i] == "PreQ9a") InApp[i] <- 1
+  if (c$variable[i] == "PreQ9b") InApp[i] <- 1
+  if (c$variable[i] == "PreQ9c") InApp[i] <- 1
+  if (c$variable[i] == "PreQ9d") InApp[i] <- 1
+  if (c$variable[i] == "PreQ9e") InApp[i] <- 1
+  if (c$variable[i] == "PreQ10a") InApp[i] <- 1
+  if (c$variable[i] == "PreQ10b") InApp[i] <- 1
+  if (c$variable[i] == "PreQ10c") InApp[i] <- 1
+  if (c$variable[i] == "PreQ10d") InApp[i] <- 1
+  if (c$variable[i] == "PostQ1a") InApp[i] <- 1
+  if (c$variable[i] == "PostQ1b") InApp[i] <- 1 
+  if (c$variable[i] == "PostQ2a") InApp[i] <- 1 
+  if (c$variable[i] == "PostQ2b") InApp[i] <- 1 
+  if (c$variable[i] == "PostQ2c") InApp[i] <- 1 
+  if (c$variable[i] == "PostQ2d") InApp[i] <- 1  
+  if (c$variable[i] == "PostQ4a") InApp[i] <- 1  
+  if (c$variable[i] == "PostQ4b") InApp[i] <- 1   
+  if (c$variable[i] == "PostQ4c") InApp[i] <- 1
+  if (c$variable[i] == "PostQ4d") InApp[i] <- 1
+  if (c$variable[i] == "PostQ5a") InApp[i] <- 1
+  if (c$variable[i] == "PostQ6a") InApp[i] <- 1  
+  if (c$variable[i] == "PostQ6b") InApp[i] <- 1 
+  if (c$variable[i] == "PostQ6d") InApp[i] <- 1 
+  if (c$variable[i] == "PostQ7a") InApp[i] <- 1
+  if (c$variable[i] == "PostQ7b") InApp[i] <- 1
+  if (c$variable[i] == "PostQ7c") InApp[i] <- 1
+  if (c$variable[i] == "PostQ7d") InApp[i] <- 1
+  if (c$variable[i] == "PostQ8a") InApp[i] <- 1
+  if (c$variable[i] == "PostQ8c") InApp[i] <- 1
+  if (c$variable[i] == "PostQ9a") InApp[i] <- 1
+  if (c$variable[i] == "PostQ9b") InApp[i] <- 1
+  if (c$variable[i] == "PostQ9c") InApp[i] <- 1
+  if (c$variable[i] == "PostQ9d") InApp[i] <- 1
+  if (c$variable[i] == "PostQ9e") InApp[i] <- 1
+  if (c$variable[i] == "PostQ10a") InApp[i] <- 1
+  if (c$variable[i] == "PostQ10b") InApp[i] <- 1
+  if (c$variable[i] == "PostQ10c") InApp[i] <- 1
+  if (c$variable[i] == "PostQ10d") InApp[i] <- 1
 }
-                             
+
 d <- cbind(c, InApp)
 head(d)
-                  
+
 # separate pre- and post- content scores
+e <- d
+
 pre <- d[grep("Pre", e$variable),]
 post <- d[grep("Post", e$variable),]
 names(pre)[names(pre)=="variable"] <- "ContentID"
@@ -158,7 +169,11 @@ names(pre)[names(pre)=="value"] <- "ContentPreScore"
 names(post)[names(post)=="value"] <- "ContentPostScore"
 ContentPostScore <- post[,13]
 g <- cbind(pre, ContentPostScore)
-attach(g)
+g$ContentPreScore<-as.numeric(g$ContentPreScore)
+head(g)
+
+
+
 
 # separate pre- and post- affect scores
 affect <- g[grep("Q12", g$ContentID),]
@@ -166,14 +181,89 @@ names(affect)[names(affect)=="ContentID"] <- "AffectID"
 names(affect)[names(affect)=="ContentPreScore"] <- "AffectPreScore"
 names(affect)[names(affect)=="ContentPostScore"] <- "AffectPostScore"
 
+
 # merge dataframes
-data <- merge(g,affe)
+data <- merge(g,affect)
 head(data)
+data[data==""] <-NA
+data <- na.omit(data)
+
+#basic patterns
+aggregate(Ex3tot~section+Gender, data, mean)
+aggregate(Ex3tot~section, data, mean)
+
+ttest_gender <- t.test(Ex3tot~Gender, data)
+ttest_section <- t.test(Ex3tot~section, data)
+boxplot(Ex3tot~section, data=data, main="Exam 3 Scores by Section", xlab="Section", ylab="Mean Exam Score")
+boxplot(Ex3tot~Gender, data=data, main="Exam 3 Scores by Gender", xlab="Gender", ylab="Mean Exam Score")
 
 ### now let's look at some *models!* ###
+modAll.1e <- lm(Ex3tot ~ UW_GPA + ContentID + SATM + SATV + ContentPreScore + SOI + section + Gender + section*SOI + section*Gender, data=data)
+# need data=data or to name each variable data$UW_GPA to avoid using attach()
+summary(modAll.1e)
+anova(modAll.1e, test="Chisq") 
 
-modAll.1e <- lm(Ex3tot ~ UW_GPA + ContentID + ContentPreScore + SOI + AC + section*SOI + section*Gender)
+# remove ContentID as least sig
+modAll.2e <- lm(Ex3tot ~ UW_GPA + SATM + SATV + ContentPreScore + SOI + section + Gender + section*SOI + section*Gender, data=data)
+summary(modAll.2e)
+anova(modAll.2e, test="Chisq") 
+
+#remove section as least significant
+modAll.3e <- lm(Ex3tot ~ UW_GPA + SATM + SATV + ContentPreScore + SOI + Gender + section*Gender, data=data)
+summary(modAll.3e)
+anova(modAll.3e, test="Chisq") 
+
+#remove SOI as least significant
+modAll.4e <- lm(Ex3tot ~ UW_GPA + SATM + SATV + ContentPreScore + Gender + section*Gender, data=data)
+summary(modAll.4e)
+anova(modAll.4e, test="Chisq") 
+
+#remove SATM as least significant
+modAll.5e <- lm(Ex3tot ~ UW_GPA + SATV + ContentPreScore + Gender + section*Gender, data=data)
+summary(modAll.5e)
+anova(modAll.5e, test="Chisq") 
+
+#remove SATV as least significant
+modAll.6e <- lm(Ex3tot ~ UW_GPA + ContentPreScore + Gender + section*Gender, data=data)
+summary(modAll.6e)
+anova(modAll.6e, test="Chisq")
+
+#remove section*Gender as least significant
+modAll.7e <- lm(Ex3tot ~ UW_GPA + ContentPreScore + Gender, data=data)
+summary(modAll.7e)
+anova(modAll.7e, test="Chisq")
+
+AIC(modAll.1e,modAll.2e,modAll.3e,modAll.4e,modAll.5e,modAll.6e,modAll.7e)
+BIC(modAll.1e,modAll.2e,modAll.3e,modAll.4e,modAll.5e,modAll.6e,modAll.7e)
 
 
 
+# need data=data or to name each variable data$UW_GPA to avoid using attach()
+
+
+
+
+
+
+
+# want to see 
+# I was having some issues with the variable AC in the above model (returning NA coefficients)
+# it isn't necessary there (affect question group is irrelevant to exam scores)
+# so I want to see if I have problems with it in the models that it will be included in:
+library(MASS)
+
+modtry <- polr(as.factor(AffectPostScore) ~ UW_GPA + AffectPreScore + AffectID + as.factor(AC) +
+                 SOI + Gender + section*SOI + section*Gender, data=data,Hess=TRUE) #, na.action=na.omit)
+summary(modtry)
+# dropping AC 
+
+modtry2 <- lm(AffectPostScore ~ UW_GPA + AffectPreScore + AffectID + as.numeric(AC) +
+                SOI + Gender + section*SOI + section*Gender, data=data)#,Hess=TRUE) #, na.action=na.omit)
+summary(modtry2)
+# dropping AC 
+
+data$AC <-
+  
+  modtry3 <- lm(AffectPostScore ~ as.factor(AC), data=data)
+summary(modtry3)
 
